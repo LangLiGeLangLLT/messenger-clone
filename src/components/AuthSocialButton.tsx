ea988c84caf5697389010953b4d5307f8eaa6a2e
llt <418797644@qq.com>
2023-05-09 21:38:50 +0800

    feat: update

---
+++ src/components/AuthSocialButton.tsx
@@ -0,0 +1,22 @@
+import { ButtonHTMLAttributes } from 'react'
+import { IconType } from 'react-icons'
+
+interface AuthSocialButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
+  icon: IconType
+}
+
+function AuthSocialButton({ icon: Icon, ...props }: AuthSocialButtonProps) {
+  const { children } = props
+  return (
+    <button
+      {...props}
+      className="inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0"
+    >
+      <Icon />
+      {children}
+    </button>
+  )
+}
+
+export default AuthSocialButton

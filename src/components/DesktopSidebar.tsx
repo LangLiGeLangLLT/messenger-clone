d79b2f24f2c113ddcbadb85f4d1a2e5171e6aa1e
llt <418797644@qq.com>
2023-05-14 17:57:13 +0800

    feat: update

--- src/components/DesktopSidebar.tsx
+++ src/components/DesktopSidebar.tsx
@@ -4,16 +4,15 @@ import useRoutes from '@/hooks/useRoutes'
 import { useState } from 'react'
 import DesktopItem from './DesktopItem'
 import { User } from '@prisma/client'
+import Avatar from './Avatar'
 
 interface DesktopSidebarProps {
-  currentUser: User | null
+  currentUser: User
 }
 
 function DesktopSidebar({ currentUser }: DesktopSidebarProps) {
   const routes = useRoutes()
   const [isOpen, setIsOpen] = useState(false)
-
-  console.log(currentUser)
 
   return (
     <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
@@ -30,6 +29,14 @@ function DesktopSidebar({ currentUser }: DesktopSidebarProps) {
             />
           ))}
         </ul>
+      </nav>
+      <nav className="mt-4 flex flex-col justify-between items-center">
+        <div
+          onClick={() => setIsOpen(true)}
+          className="cursor-pointer hover:opacity-75 transition"
+        >
+          <Avatar user={currentUser} />
+        </div>
       </nav>
     </div>
   )

9db0ee8413f93cbdaa5205afb2438c6247fbd5b7
llt <418797644@qq.com>
2023-05-18 00:54:29 +0800

    feat: update

--- src/hooks/useActiveChannel.ts
+++ src/hooks/useActiveChannel.ts
@@ -27,7 +27,7 @@ const useActiveChannel = () => {
       add(member.id)
     })
 
-    channel.bind('pusher:member_remove', (member: Record<string, any>) => {
+    channel.bind('pusher:member_removed', (member: Record<string, any>) => {
       remove(member.id)
     })
 
